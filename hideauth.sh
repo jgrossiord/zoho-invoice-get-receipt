37f6605e44169761bf9b2b6dabcb98900e54085d
Julien Grossiord <julien@grossiord.net>
2014-04-22 14:15:53 +0200

    retest hideauth

--- hideauth.sh
+++ hideauth.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-awk '{ if (/authToken = /) print "authToken = [AUTHTOKEN]"; else print $0; }'
-awk '{ if (/organizationId = /) print "organizationId = [ORGANIZATIONID]"; else print $0; }'
+awk '{ if (/authToken = /) print "authToken = [AUTHTOKEN]"; else if (/organizationId = /) print "organizationId = [ORGANIZATIONID]"; else print $0; }'
 exit 0
