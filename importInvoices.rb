08ab46846c32beb375022e6d406e13f06dc9efe4
Julien Grossiord <julien@grossiord.net>
2014-04-25 10:34:15 +0200

    Only try to download receipt when it exists

--- importInvoices.rb
+++ importInvoices.rb
@@ -31,48 +31,50 @@ data = RestClient.get expensesEndPointApi, {:params => {:authtoken => authToken,
 data = JSON.parse(data)
 
 data["expenses"].each do |line|
-	expense = JSON.parse(RestClient.get expensesEndPointApi + '/' + line["expense_id"], {:params => {:authtoken => authToken, :organization_id => organizationId}})
-	expense = expense["expense"]
-	fileName = ""
-	filePath = ""
-	if localPathWithBillable && expense["is_billable"]
-		filePath = "billable/"
-	elsif localPathWithBillable
-		filePath = "nonbillable/"
-	end
-
-	if localPathWithCustomerName && expense["customer_name"] != ""
-		filePath = filePath + expense["customer_name"] + "/"
-	end
-
-	if localPathWithProjectName && expense["project_name"] != ""
-		filePath = filePath + expense["project_name"] + "/"
-	end
-
-	if localPathWithYearMonth
-		d = Date.parse(expense["date"])
-		if d.month < 10
-			month = "0" + d.month.to_s
-		else
-			month = d.month.to_s
+	if line["expense_receipt_name"] != ""
+		expense = JSON.parse(RestClient.get expensesEndPointApi + '/' + line["expense_id"], {:params => {:authtoken => authToken, :organization_id => organizationId}})
+		expense = expense["expense"]
+		fileName = ""
+		filePath = ""
+		if localPathWithBillable && expense["is_billable"]
+			filePath = "billable/"
+		elsif localPathWithBillable
+			filePath = "nonbillable/"
 		end
-		filePath = filePath + d.year.to_s + "-" + month + "/"
-	end
 
-	FileUtils::mkdir_p localReceiptPath + filePath
+		if localPathWithCustomerName && expense["customer_name"] != ""
+			filePath = filePath + expense["customer_name"] + "/"
+		end
 
-	fileName = filePath + expense["date"] + "_" + expense["expense_id"] + "_" + expense["expense_receipt_name"]
+		if localPathWithProjectName && expense["project_name"] != ""
+			filePath = filePath + expense["project_name"] + "/"
+		end
 
-	url = expensesEndPointApi + "/" + expense["expense_id"] + "/receipt?organization_id=" + organizationId + "&authtoken=" + authToken
-
-	unless File.exists?(localReceiptPath+fileName)
-		puts "Downloading " + fileName 
-		File.open(localReceiptPath+fileName, "wb") do |saved_file|
-			open(url, "rb") do |read_file|
-				saved_file.write(read_file.read)
+		if localPathWithYearMonth
+			d = Date.parse(expense["date"])
+			if d.month < 10
+				month = "0" + d.month.to_s
+			else
+				month = d.month.to_s
 			end
+			filePath = filePath + d.year.to_s + "-" + month + "/"
 		end
-	else
-		puts fileName + " already exists locally"
+
+		FileUtils::mkdir_p localReceiptPath + filePath
+
+		fileName = filePath + expense["date"] + "_" + expense["expense_id"] + "_" + expense["expense_receipt_name"]
+
+		url = expensesEndPointApi + "/" + expense["expense_id"] + "/receipt?organization_id=" + organizationId + "&authtoken=" + authToken
+
+		unless File.exists?(localReceiptPath+fileName)
+			puts "Downloading " + fileName 
+			File.open(localReceiptPath+fileName, "wb") do |saved_file|
+				open(url, "rb") do |read_file|
+					saved_file.write(read_file.read)
+				end
+			end
+		else
+			puts fileName + " already exists locally"
+		end
 	end
 end
